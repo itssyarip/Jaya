7660d8db3c06036dcf75b21e323ad9772fc2a3ab
agra kurnia <tukangj4l4n@gmail.com>
2017-10-19 13:24:27 +0700

    edit

--- application/views/login.php
+++ application/views/login.php
@@ -4,25 +4,13 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-<<<<<<< HEAD
         <title>SIA PT Jaya</title>
-=======
-        <title>Limitless - Responsive Web Application Kit by Eugene Kopyov</title>
->>>>>>> 1062852dd398d01bf9af2bbac9eae803c4f0d490
-
         <?php
         $this->load->view("util/globalJs");
         $this->load->view("util/formCss");
         ?>
-        
-
-	
     </head>
     <body class="login-container">
-
-<<<<<<< HEAD
- 
-=======
         <!-- Main navbar -->
         <div class="navbar navbar-inverse bg-indigo">
             <div class="navbar-header">
@@ -57,9 +45,6 @@
             </div>
         </div>
         <!-- /main navbar -->
->>>>>>> 1062852dd398d01bf9af2bbac9eae803c4f0d490
-
-
         <!-- Page container -->
         <div class="page-container">
             <!-- Page content -->
@@ -70,11 +55,8 @@
                     <form class="login-form" action="#" id="form_login">
                         <div class="panel panel-body login-form">
                             <div class="text-center">
-<<<<<<< HEAD
                                 <div class="icon-object border-slate-300 text-slate-300"><img style="width: 70px;" src="https://ecc.ft.ugm.ac.id/public/employer_logo/150289/large_1390964213jaya-properti.png"></div>
-=======
-                                <div class="icon-object border-slate-300 text-slate-300"><i class="icon-reading"></i></div>
->>>>>>> 1062852dd398d01bf9af2bbac9eae803c4f0d490
+
                                 <h5 class="content-group">Login to your account <small class="display-block">Enter your credentials below</small></h5>
                                 <div class="alert alert-error" style="display:none;">
                                     <button class="close" data-dismiss="alert"></button>
@@ -107,21 +89,6 @@
                                 </div>
                             </div>
                             <div class="form-group">
-<<<<<<< HEAD
-                                <button type="submit" class="btn bg-danger btn-block">Sign in <i class="icon-circle-right2 position-right"></i></button>
-                            </div>
-
-                            <div class="text-center">
-                                <!-- <a href="login_password_recover.html">Forgot password?</a> -->
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        <div class="footer text-muted text-center">
-            &copy; 2017. <a href="#">SIA - PT Jaya Real Property</a> by <a href="http://http://octatech.co.id/" target="_blank">octatech.co.id</a>
-=======
                                 <button type="submit" class="btn bg-pink-400 btn-block">Sign in <i class="icon-circle-right2 position-right"></i></button>
                             </div>
 
@@ -144,8 +111,7 @@
 
         <!-- Footer -->
         <div class="footer text-muted text-center">
-            &copy; 2015. <a href="#">Limitless Web App Kit</a> by <a href="http://themeforest.net/user/Kopyov" target="_blank">Eugene Kopyov</a>
->>>>>>> 1062852dd398d01bf9af2bbac9eae803c4f0d490
+            &copy; 2017. <a href="#">SIA - PT Jaya Real Property</a> by <a href="http://http://octatech.co.id/" target="_blank">octatech.co.id</a>
         </div>
         <!-- /footer -->
         <script src="<?= base_url() ?>assets/jquery.validate.min.js" type="text/javascript"></script>
